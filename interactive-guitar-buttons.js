2eb531315852cdcc7f3aa6642e8b9928880edda1
Kirk Morgan <mrkirkmorgan@gmail.com>
2020-05-28 23:30:07 -0400

    xcvzxc

--- interactive-guitar-buttons.js
+++ interactive-guitar-buttons.js
@@ -51,73 +51,8 @@ let chordFormulas = {
   "Suspended": ["P4, M2"]
 }
 
-let allChords = [
-  [ 'E Suspended', [ 'A', 'B', 'E' ] ],
-  [ 'A Diminished', [ 'A', 'C', 'D#' ] ],
-  [ 'A Minor', [ 'A', 'C', 'E' ] ],
-  [ 'F Major', [ 'A', 'C', 'F' ] ],
-  [ 'F# Diminished', [ 'A', 'C', 'F#' ] ],
-  [ 'A Major', [ 'A', 'C#', 'E' ] ],
-  [ 'A Augmented', [ 'A', 'C#', 'F' ] ],
-  [ 'C# Augmented', [ 'A', 'C#', 'F' ] ],
-  [ 'F Augmented', [ 'A', 'C#', 'F' ] ],
-  [ 'F# Minor', [ 'A', 'C#', 'F#' ] ],
-  [ 'A Suspended', [ 'A', 'D', 'E' ] ],
-  [ 'D Minor', [ 'A', 'D', 'F' ] ],
-  [ 'D Major', [ 'A', 'D', 'F#' ] ],
-  [ 'D Suspended', [ 'A', 'D', 'G' ] ],
-  [ 'D# Diminished', [ 'A', 'D#', 'F#' ] ],
-  [ 'F Suspended', [ 'A#', 'C', 'F' ] ],
-  [ 'A# Diminished', [ 'A#', 'C#', 'E' ] ],
-  [ 'A# Minor', [ 'A#', 'C#', 'F' ] ],
-  [ 'F# Major', [ 'A#', 'C#', 'F#' ] ],
-  [ 'G Diminished', [ 'A#', 'C#', 'G' ] ],
-  [ 'A# Major', [ 'A#', 'D', 'F' ] ],
-  [ 'A# Augmented', [ 'A#', 'D', 'F#' ] ],
-  [ 'D Augmented', [ 'A#', 'D', 'F#' ] ],
-  [ 'F# Augmented', [ 'A#', 'D', 'F#' ] ],
-  [ 'G Minor', [ 'A#', 'D', 'G' ] ],
-  [ 'A# Suspended', [ 'A#', 'D#', 'F' ] ],
-  [ 'D# Minor', [ 'A#', 'D#', 'F#' ] ],
-  [ 'D# Major', [ 'A#', 'D#', 'G' ] ],
-  [ 'D# Suspended', [ 'A#', 'D#', 'G#' ] ],
-  [ 'E Diminished', [ 'A#', 'E', 'G' ] ],
-  [ 'F# Suspended', [ 'B', 'C#', 'F#' ] ],
-  [ 'B Diminished', [ 'B', 'D', 'F' ] ],
-  [ 'B Minor', [ 'B', 'D', 'F#' ] ],
-  [ 'G Major', [ 'B', 'D', 'G' ] ],
-  [ 'G# Diminished', [ 'B', 'D', 'G#' ] ],
-  [ 'B Major', [ 'B', 'D#', 'F#' ] ],
-  [ 'B Augmented', [ 'B', 'D#', 'G' ] ],
-  [ 'D# Augmented', [ 'B', 'D#', 'G' ] ],
-  [ 'G Augmented', [ 'B', 'D#', 'G' ] ],
-  [ 'G# Minor', [ 'B', 'D#', 'G#' ] ],
-  [ 'B Suspended', [ 'B', 'E', 'F#' ] ],
-  [ 'E Minor', [ 'B', 'E', 'G' ] ],
-  [ 'E Major', [ 'B', 'E', 'G#' ] ],
-  [ 'F Diminished', [ 'B', 'F', 'G#' ] ],
-  [ 'G Suspended', [ 'C', 'D', 'G' ] ],
-  [ 'C Diminished', [ 'C', 'D#', 'F#' ] ],
-  [ 'C Minor', [ 'C', 'D#', 'G' ] ],
-  [ 'G# Major', [ 'C', 'D#', 'G#' ] ],
-  [ 'C Major', [ 'C', 'E', 'G' ] ],
-  [ 'C Augmented', [ 'C', 'E', 'G#' ] ],
-  [ 'E Augmented', [ 'C', 'E', 'G#' ] ],
-  [ 'G# Augmented', [ 'C', 'E', 'G#' ] ],
-  [ 'C Suspended', [ 'C', 'F', 'G' ] ],
-  [ 'F Minor', [ 'C', 'F', 'G#' ] ],
-  [ 'G# Suspended', [ 'C#', 'D#', 'G#' ] ],
-  [ 'C# Diminished', [ 'C#', 'E', 'G' ] ],
-  [ 'C# Minor', [ 'C#', 'E', 'G#' ] ],
-  [ 'C# Major', [ 'C#', 'F', 'G#' ] ],
-  [ 'C# Suspended', [ 'C#', 'F#', 'G#' ] ],
-  [ 'D Diminished', [ 'D', 'F', 'G#' ] ]
-]
-
-let directory = {
-  3: { A: 0, 'A#': 15, B: 30, C: 44, 'C#': 54, D: 59 }
-}
-
+let allChords = [];
+let directory = {};
 
 function locateChord(chord) {
   if (Object.keys(directory).includes(chord.length.toString())) {
@@ -313,4 +248,112 @@ function calculateNote() {
   }
 
   return locateChord(notes);
+}
+
+let allChords = [];
+let directory = {};
+
+generateChords();
+createDirectory();
+
+function generateChords() {
+  let chords = [];
+
+  for (let type in chordFormulas) {
+    for (let note of chromaticScale) {
+      var chord = [note];
+      var index = chromaticScale.indexOf(note);
+      var interval = chordFormulas[type];
+
+      for (let i = 0; i < interval.length; i++) {
+        index += intervals[interval[i]];
+        index %= chromaticScale.length;
+        chord.push(chromaticScale[index]);
+      }
+
+      let sortedChord = sortChord(chord);
+      chords.push([note + " " + type, sortedChord]);
+    }
+  }
+
+  for (var chord of chords) {
+    if (allChords.length == 0) {
+      allChords.push(chord);
+    } else {
+      let placed = false;
+      for (var j = 0; j < allChords.length; j++) {
+
+        // Test against the first
+        if (compareChords(chord[1], allChords[j][1])) {
+          allChords.splice(j, 0, chord);
+          placed = true;
+          break;
+        }
+      }
+
+      if (!placed) {
+        allChords.push(chord)
+      }
+    }
+  }
+}
+
+// Compares chord1 to chord2. If -1 chord1 is smaller, if 0 theyre the same
+function compareChords(chordOne, chordTwo) {
+  if (chordOne.length < chordTwo.length) {
+    return true;
+  } else if (chordOne.length == chordTwo.length) {
+    for (let  i = 0; i < chordOne.length; i++) {
+      if (chromaticScale.indexOf(chordOne[i]) < chromaticScale.indexOf(chordTwo[i])) {
+        return true;
+      } else if (chromaticScale.indexOf(chordOne[i]) > chromaticScale.indexOf(chordTwo[i])) {
+        return false;
+      }
+    }
+    return false;
+  } else {
+    return false;
+  }
+}
+
+function sortChord(chord) {
+  for (var i = 1; i < chord.length; i++) {
+    let note = chord[i];
+    let prevNote = chord[i - 1];
+    let adjust = 0;
+
+    if (chromaticScale.indexOf(note) < chromaticScale.indexOf(prevNote)) {
+      while (chromaticScale.indexOf(note) < chromaticScale.indexOf(prevNote)) {
+        adjust++;
+        prevNote = chord[i - 1 - adjust];
+      }
+      chord.splice(i, 1);
+      chord.splice(i - adjust, 0, note);
+    }
+  }
+
+  return chord;
+}
+
+function createDirectory() {
+  let currNote = "A";
+  let currLength = 3;
+
+  directory[currLength] = {"A": 0};
+
+  for (let i = 0; i < allChords.length; i++) {
+    let chord = allChords[i][1];
+
+    if (chord.length != currLength) {
+      currLength++;
+      directory[currLength] = {};
+    }
+
+    if (chord[0] != currNote) {
+      let currIndex = chromaticScale.indexOf(currNote);
+      let nextNote = chromaticScale[(currIndex + 1) % chromaticScale.length]
+      directory[currLength][nextNote] = i;
+      currNote = nextNote;
+    }
+  }
 }
